8a88f4124aec26ac1de66b898c36462ce0d26e2a
Dave Moore <dmoore@nexus.a118.uk>
2026-06-30 23:03:06 +0000

    auto: new stream 7YzD5Zly_RE

--- stream/stream.js
+++ stream/stream.js
@@ -1,3 +1,3 @@
 // Update this ID when the stream URL changes.
 // Find it in the YouTube URL: youtube.com/live/VIDEO_ID
-const STREAM_VIDEO_ID = "omkbPDYLo1o";
+const STREAM_VIDEO_ID = "7YzD5Zly_RE";
